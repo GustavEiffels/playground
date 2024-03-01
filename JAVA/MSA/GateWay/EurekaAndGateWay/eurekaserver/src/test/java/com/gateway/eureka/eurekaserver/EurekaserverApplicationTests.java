4054c448a4eaff23982c172b773ab11f772b99e1
GustavEiffels <sibuk9315@gmail.com>
2024-03-01 16:44:57 +0900

    Add Eureka

---
+++ JAVA/MSA/GateWay/EurekaAndGateWay/eurekaserver/src/test/java/com/gateway/eureka/eurekaserver/EurekaserverApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gateway.eureka.eurekaserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EurekaserverApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
